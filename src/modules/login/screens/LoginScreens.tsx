dbe0481a3d4f7dd342927b0c5b086c7bc2a51af4
Luisdney <luisdney1@gmail.com>
2024-01-22 23:39:23 -0300

    feat: create module login

---
+++ src/modules/login/screens/LoginScreens.tsx
@@ -0,0 +1,9 @@
+import { BackgroundImage } from "../styles/loginScreens.styles";
+
+const LoginScreen = () => {
+  return (<div>
+    <BackgroundImage src="./background.png" />
+  </div>)
+}
+
+export default LoginScreen;
